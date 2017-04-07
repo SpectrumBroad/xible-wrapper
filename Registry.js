087fa2ca9ce9dd1b5feadfe1aa34fbbbabb6ad62
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-04-07 14:07:51 +0200

    higher timeout for registry installation calls

--- Registry.js
+++ Registry.js
@@ -2,22 +2,23 @@ module.exports = function(XIBLE) {
 
 	class Registry {
 
-    static searchNodePacks(searchString) {
+		static searchNodePacks(searchString) {
 
-      let req = XIBLE.httpBase.request('GET', `http${XIBLE.baseUrl}/api/registry/nodepacks?search=${encodeURIComponent(searchString)}`);
+			let req = XIBLE.httpBase.request('GET', `http${XIBLE.baseUrl}/api/registry/nodepacks?search=${encodeURIComponent(searchString)}`);
 			return req.toJson();
 
-    }
+		}
 
 		static installNodePackByName(nodePackName) {
 
 			let req = XIBLE.httpBase.request('PATCH', `http${XIBLE.baseUrl}/api/registry/nodepacks/${encodeURIComponent(nodePackName)}/install`);
+			req.timeout = 120000; //give this a high timeout because installing may take a while
 			return req.send();
 
 		}
 
-  }
+	}
 
-  return Registry;
+	return Registry;
 
 };
