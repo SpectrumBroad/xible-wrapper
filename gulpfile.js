8408d0482722a2db9e8dd960118aecbcec2db261
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-06-04 16:02:42 +0200

    ready for publish

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,16 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
 const transform = require('vinyl-source-stream');
-const rename = require('gulp-rename');
 
 gulp.task('browserify', () =>
   browserify('index.js', {
     standalone: 'XibleWrapper'
   })
-  .bundle()
-  .pipe(transform('index.js'))
-  .pipe(gulp.dest('./dist')));
-
-gulp.task('toXibleEditor', ['browserify'], () =>
-  gulp.src('dist/index.js')
-  .pipe(rename('xibleWrapper.js'))
-  .pipe(gulp.dest('../xible/editor/js/')));
+    .bundle()
+    .pipe(transform('index.js'))
+    .pipe(gulp.dest('./dist'))
+);
 
 gulp.task('start', () => {
   gulp.watch(['*.js'], ['browserify']);
-});
-
-gulp.task('xibleeditor', ['toXibleEditor'], () => {
-  gulp.watch(['*.js'], ['toXibleEditor']);
 });
