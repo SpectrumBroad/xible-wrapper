5fd822b46fcce802754a5af340efefa78371f399
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-06-05 13:12:40 +0200

    cache config and handle config socket events

--- Config.js
+++ Config.js
@@ -1,11 +1,22 @@
 'use strict';
 
 module.exports = (XIBLE) => {
+  // local cache
+  let config = null;
+
   class Config {
 
     static getAll() {
-      const req = XIBLE.httpBase.request('GET', `http${XIBLE.baseUrl}/api/config`);
-      return req.toJson();
+      if (config) {
+        return Promise.resolve(config);
+      }
+
+      return XIBLE.httpBase.request('GET', `http${XIBLE.baseUrl}/api/config`)
+        .toJson()
+        .then((configObj) => {
+          config = configObj;
+          return config;
+        });
     }
 
     static validatePermissions() {
@@ -14,6 +25,8 @@ module.exports = (XIBLE) => {
     }
 
     static deleteValue(path) {
+      this.deleteObjectValueOnPath(config, path);
+
       const req = XIBLE.httpBase.request('DELETE', `http${XIBLE.baseUrl}/api/config/value`);
       return req.toJson({
         path
@@ -25,11 +38,46 @@ module.exports = (XIBLE) => {
         throw new Error('Param "value" should be of type "string", "number", "boolean" or "date"');
       }
 
+      this.setObjectValueOnPath(config, path, value);
+
       const req = XIBLE.httpBase.request('PUT', `http${XIBLE.baseUrl}/api/config/value`);
       return req.toJson({
         path,
         value
       });
+    }
+
+    static deleteObjectValueOnPath(obj, path) {
+      const pathSplit = path.split('.');
+      let sel = obj;
+
+      for (let i = 0; i < pathSplit.length - 1; i += 1) {
+        const part = pathSplit[i];
+        if (sel.hasOwnProperty(part)) {
+          sel = sel[part];
+        } else {
+          return false;
+        }
+      }
+
+      delete sel[pathSplit.pop()];
+      return true;
+    }
+
+    static setObjectValueOnPath(obj, path, value) {
+      const pathSplit = path.split('.');
+      let sel = obj;
+
+      for (let i = 0; i < pathSplit.length - 1; i += 1) {
+        const part = pathSplit[i];
+        if (sel.hasOwnProperty(part)) {
+          sel = sel[part];
+        } else {
+          sel = sel[part] = {};
+        }
+      }
+
+      sel[pathSplit.pop()] = value;
     }
 
     static getObjectValueOnPath(obj, path) {
@@ -50,10 +98,32 @@ module.exports = (XIBLE) => {
 
     static getValue(path) {
       return this.getAll()
-        .then(config => this.getObjectValueOnPath(config, path));
+        .then(configObj => this.getObjectValueOnPath(configObj, path));
     }
 
   }
+
+  // reset config when the connection closes
+  XIBLE.on('close', () => {
+    config = null;
+  });
+
+  // handle socket messages
+  XIBLE.on('message', (json) => {
+    if (!config) {
+      return;
+    }
+
+    switch (json.method) {
+      case 'xible.config.setValue':
+        Config.setObjectValueOnPath(config, json.path, json.value);
+        break;
+
+      case 'xible.config.deleteValue':
+        Config.deleteObjectValueOnPath(config, json.path);
+        break;
+    }
+  });
 
   return Config;
 };
