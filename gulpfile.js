c27eb177eb881956e37cff6f3d807684d23e3b59
Jong <MV96TI@ad.ing.net>
2018-04-29 09:12:38 +0200

    change start to default task

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,6 @@ gulp.task('browserify', () =>
   .pipe(gulp.dest('./dist'))
 );
 
-gulp.task('start', ['browserify'], () => {
+gulp.task('default', ['browserify'], () => {
   gulp.watch(['*.js'], ['browserify']);
 });
